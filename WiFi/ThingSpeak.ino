62bd4345ca706322d0c780d621ffc28c0e0eec98
David Dubins <d.dubins@utoronto.ca>
2025-05-04 14:54:16 -0400

    Update ThingSpeak.ino

--- WiFi/ThingSpeak.ino
+++ WiFi/ThingSpeak.ino
@@ -1,11 +1,11 @@
 //ThingSpeak.ino: Report one or more values to https://thingspeak.mathworks.com
 //Author: D. Dubins
 //Date: 04-May-25
+
 #include <ESP8266WiFi.h>
 // WiFi setup:
 const char* ssid = "mySSID";       // replace with your WiFi network ssid
-const char* password = "myPASSWORD";  // replace with your WiFi passward
-
+const char* password = "myPASSWORD";  // replace with your WiFi password
 WiFiClient client;
 
 // Thingspeak setup:
@@ -15,16 +15,8 @@ const char* server = "api.thingspeak.com";  // thingspeak server
 
 void setup() {
   Serial.begin(115200);  //start Serial Monitor
-  delay(5000);           // Wait a bit
-  WiFi.begin(ssid, password);
-  Serial.print("\n\nConnecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi connected");
+  delay(5000);           // wait a bit
+  connectToWiFi();  // connect to WiFi
 }
 
 void loop() {
@@ -46,10 +38,10 @@ void loop() {
     client.print(postStr.length());
     client.print("\n\n");
     client.print(postStr);
-    Serial.print("Measure 1 sent: ");  // revise as necessary for the number of fields used
-    Serial.print(t); // add data for field1
+    Serial.print("Measure 1 sent: ");  // print measurements to Serial Monitor (debugging)
+    Serial.print(t); // data for field1
     //Serial.print(", Measure 2: ");
-    //Serial.print(v); // send field2 data
+    //Serial.print(v); // data for field2 data
     Serial.println(""); // add LN for end of Serial Monitor print
     //Wait for server response.
     //The following code was created with the assistance of ChatGPT 4.0, an AI language model by OpenAI.
@@ -80,4 +72,18 @@ void loop() {
 
   client.stop();
   delay(60000);  // 60 sec delay. thingspeak needs min 15 sec delay betw updates
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWiFi();  // reconnect if WiFi is disconnected
+  }
+}
+
+void connectToWiFi() {
+  Serial.print("\n\nConnecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
 }
